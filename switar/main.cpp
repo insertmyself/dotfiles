bc913d922c565f84e917eecf3f9e698d64eb6a47
Wetar <azrilharis75@gmail.com>
2026-07-17 19:46:52 +0700

    update(niri): removed the wallpaper setter from the repo

--- switar/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "modules/headers/switar.h"
-#include <QApplication>
-
-int main(int argc, char *argv[]) {
-  QApplication app(argc, argv);
-  Switar window;
-  window.show();
-  return app.exec();
-}
